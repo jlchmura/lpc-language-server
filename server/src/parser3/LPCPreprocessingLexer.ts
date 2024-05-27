fca3662bc95f3b5a8ded8c86b5820c434a300e9e
jlchmura <jlchmura@gmail.com>
2024-05-27 09:21:06 -0400

    refactor to organize code

--- server/src/parser3/LPCPreprocessingLexer.ts
+++ server/src/parser3/LPCPreprocessingLexer.ts
@@ -192,6 +192,260 @@ export class LPCPreprocessingLexer extends LPCLexer {
         return consumedTokens;
     }
 
+    private processInclude(
+        token: Token,
+        directiveToken: Token,
+        directiveTokens: Token[]
+    ): boolean {
+        directiveTokens.shift(); // remove the space
+        const includeFilename = directiveTokens
+            .map((t) => t.text)
+            .join(" ")
+            .trim();
+
+        if (!this.fileHandler) {
+            console.warn(
+                `Could not load ${includeFilename}, no file handler set.`
+            );
+        }
+
+        const includeFile = this.fileHandler.loadImport(includeFilename);
+
+        (this.macroLexer.tokenFactory as LPCTokenFactor).filenameStack.push(
+            includeFile.uri
+        );
+
+        // lex the the include file
+        this.macroLexer.inputStream = CharStream.fromString(includeFile.source);
+        this.macroLexer.reset();
+        const includeTokens = this.macroLexer.getAllTokens();
+
+        // check if last token is EOF and remove it
+        if (includeTokens[includeTokens.length - 1].type == LPCLexer.EOF) {
+            includeTokens.pop();
+        }
+
+        // push via emit to ensure that the tokens are processed by the preprocessor
+        this.isConsumingDirective = false;
+        this.allowSubstitutions = true;
+
+        this.emitQueue.push(...includeTokens);
+        // includeTokens.forEach((t) => {
+        //     this.emitToken(t);
+        // });
+
+        (this.macroLexer.tokenFactory as LPCTokenFactor).filenameStack.pop();
+        return true;
+    }
+
+    private processDefine(
+        token: Token,
+        directiveToken: Token,
+        directiveTokens: Token[]
+    ): boolean {
+        const defValToken = directiveTokens.shift()!;
+        const defVal = defValToken.text.trim();
+        // macro name is everything up to the first space
+        const nameEndIndex = defVal.match(/[(\s]/)?.index;
+        const spaceIndex = defVal.indexOf(" ");
+        const openParenIndex = defVal.indexOf("(");
+        const closingParenIndex = defVal.indexOf(")");
+
+        const macroName = defVal.substring(0, nameEndIndex);
+
+        // macro value is everything after the first space
+        const isFn = closingParenIndex >= 0;
+        const macroValue = defVal.substring(
+            defVal.indexOf(" ", closingParenIndex)
+        );
+
+        const def: MacroDefinition = {
+            value: macroValue,
+            name: macroName,
+            filename: "macro.h",
+            annotation: "",
+            args: isFn ? [] : undefined,
+        };
+
+        // lex the body
+        this.macroLexer.inputStream = CharStream.fromString(macroValue);
+        this.macroLexer.reset();
+        def.bodyTokens = this.macroLexer.getAllTokens();
+
+        if (isFn) {
+            def.argIndex = new Map();
+
+            // lex the args
+            const args = defVal
+                .substring(openParenIndex + 1, closingParenIndex)
+                .split(",");
+            args.forEach((a, i) => {
+                def.argIndex.set(a.trim(), i);
+            });
+            // prob don't need this:
+            def.args = args.map((a) => a.trim());
+        }
+
+        this.macroTable.set(macroName, def);
+
+        return true;
+    }
+
+    private processIf(
+        token: Token,
+        directiveToken: Token,
+        directiveTokens: Token[]
+    ): boolean {
+        this.allowSubstitutions = false;
+
+        if (this.inConditional && !this.isExecutable) {
+            this.conditionalStack.push(ConditionalState.Ignored);
+            // disable this entire conditional
+            token.channel = directiveToken.channel = DISABLED_CHANNEL;
+            directiveTokens.forEach((t) => {
+                t.channel = DISABLED_CHANNEL;
+            });
+        } else {
+            const ifTokens = directiveTokens;
+            ifTokens.shift(); // remove the space
+
+            // very basic processing
+            // NTBLA: improve this
+            let expStr = ifTokens
+                .map((t) => t.text)
+                .join(" ")
+                .trim();
+
+            if (this.macroTable.has(expStr)) {
+                expStr = this.macroTable.get(expStr).value;
+            }
+
+            let flag = true;
+            if (expStr == "0") {
+                flag = false;
+            } else if (expStr == "1") {
+                flag = true;
+            } else if (expStr.includes("defined(")) {
+                flag = true; // NTBLA implement this
+            } else {
+                // some other string
+                flag = true; // NTBLA implement this
+            }
+
+            this.conditionalStack.push(
+                flag ? ConditionalState.Enabled : ConditionalState.Disabled
+            );
+
+            // hide everything
+            token.channel = directiveToken.channel = LPCLexer.HIDDEN;
+            directiveTokens.forEach((t) => {
+                t.channel = token.channel;
+            });
+        }
+        this.allowSubstitutions = true;
+        return true;
+    }
+
+    private processElse(
+        token: Token,
+        directiveToken: Token,
+        directiveTokens: Token[]
+    ): boolean {
+        this.allowSubstitutions = true;
+        if (this.inConditional) {
+            const st = this.conditionalStack[this.conditionalStack.length - 1];
+            if (st !== ConditionalState.Ignored) {
+                this.conditionalStack[this.conditionalStack.length - 1] =
+                    st === ConditionalState.Enabled
+                        ? ConditionalState.Disabled
+                        : ConditionalState.Enabled;
+            }
+        } else {
+            // this is an error - log it
+            const ltoken = directiveToken as LPCToken;
+            console.error(
+                "found an #ELSE outside of conditional directive block",
+                `${ltoken.filename}:${token.line}:${token.column}`
+            );
+        }
+        this.allowSubstitutions = true;
+        // hide everything
+        token.channel = directiveToken.channel = LPCLexer.HIDDEN;
+        directiveTokens.forEach((t) => {
+            t.channel = token.channel;
+        });
+        return true;
+    }
+
+    private processEndif(
+        token: Token,
+        directiveToken: Token,
+        directiveTokens: Token[]
+    ): boolean {
+        this.conditionalStack.pop();
+        // set this back to NOT disabled
+        token.channel = directiveToken.channel = LPCLexer.DEFAULT_TOKEN_CHANNEL;
+        // hide everything
+        token.channel = directiveToken.channel = LPCLexer.HIDDEN;
+        directiveTokens.forEach((t) => {
+            t.channel = token.channel;
+        });
+        return true;
+    }
+
+    private processIfdef(
+        token: Token,
+        directiveToken: Token,
+        directiveTokens: Token[]
+    ): boolean {
+        this.allowSubstitutions = true;
+        const conditionalTokens = directiveTokens;
+        if (this.inConditional && !this.isExecutable) {
+            this.conditionalStack.push(ConditionalState.Ignored);
+            // disable this entire conditional
+            token.channel = directiveToken.channel = DISABLED_CHANNEL;
+            conditionalTokens.forEach((t) => {
+                token.channel = DISABLED_CHANNEL;
+            });
+        } else {
+            const shouldExist = token.type == LPCLexer.IFDEF ? true : false;
+            const conditionalTokens = directiveTokens;
+            conditionalTokens.shift(); // remove the space
+            const symName = conditionalTokens
+                .map((t) => t.text)
+                .join(" ")
+                .trim();
+
+            this.conditionalStack.push(
+                this.macroTable.has(symName) === shouldExist
+                    ? ConditionalState.Enabled
+                    : ConditionalState.Disabled
+            );
+        }
+        this.allowSubstitutions = false;
+
+        // hide everything
+        token.channel = directiveToken.channel = LPCLexer.HIDDEN;
+        directiveTokens.forEach((t) => {
+            t.channel = token.channel;
+        });
+
+        return true;
+    }
+
+    private processUndefine(
+        token: Token,
+        directiveToken: Token,
+        directiveTokens: Token[]
+    ): boolean {
+        const spaceToken = directiveTokens.shift()!;
+        const undefToken = directiveTokens.shift()!;
+        const undefMacroName = undefToken.text.trim();
+        this.macroTable.delete(undefMacroName);
+
+        return true;
+    }
+
     /**
      * handles preprocessor directives
      * @param token the first token - should be a hash
@@ -207,136 +461,26 @@ export class LPCPreprocessingLexer extends LPCLexer {
         const directiveToken =
             token.type == LPCLexer.HASH ? directiveTokens.shift() : token; // special case for define
 
-        const ltoken = directiveToken as LPCToken;
-
         // there are certain conditionals that are applied even if code is disabled
         switch (directiveToken.type) {
             case LPCLexer.IF:
             case LPCLexer.ELIF:
-                this.allowSubstitutions = false;
-
-                if (this.inConditional && !this.isExecutable) {
-                    this.conditionalStack.push(ConditionalState.Ignored);
-                    // disable this entire conditional
-                    token.channel = directiveToken.channel = DISABLED_CHANNEL;
-                    directiveTokens.forEach((t) => {
-                        t.channel = DISABLED_CHANNEL;
-                    });
-                } else {
-                    const ifTokens = directiveTokens;
-                    ifTokens.shift(); // remove the space
-
-                    // very basic processing
-                    // NTBLA: improve this
-                    let expStr = ifTokens
-                        .map((t) => t.text)
-                        .join(" ")
-                        .trim();
-
-                    if (this.macroTable.has(expStr)) {
-                        expStr = this.macroTable.get(expStr).value;
-                    }
-
-                    let flag = true;
-                    if (expStr == "0") {
-                        flag = false;
-                    } else if (expStr == "1") {
-                        flag = true;
-                    } else if (expStr.includes("defined(")) {
-                        flag = true; // NTBLA implement this
-                    } else {
-                        // some other string
-                        flag = true; // NTBLA implement this
-                    }
-
-                    this.conditionalStack.push(
-                        flag
-                            ? ConditionalState.Enabled
-                            : ConditionalState.Disabled
-                    );
-
-                    // hide everything
-                    token.channel = directiveToken.channel = LPCLexer.HIDDEN;
-                    directiveTokens.forEach((t) => {
-                        t.channel = token.channel;
-                    });
-                }
-                this.allowSubstitutions = true;
-                return true;
+                return this.processIf(token, directiveToken, directiveTokens);
             case LPCLexer.ELSE:
-                this.allowSubstitutions = true;
-                if (this.inConditional) {
-                    const st =
-                        this.conditionalStack[this.conditionalStack.length - 1];
-                    if (st !== ConditionalState.Ignored) {
-                        this.conditionalStack[
-                            this.conditionalStack.length - 1
-                        ] =
-                            st === ConditionalState.Enabled
-                                ? ConditionalState.Disabled
-                                : ConditionalState.Enabled;
-                    }
-                } else {
-                    // this is an error - log it
-                    console.error(
-                        "found an #ELSE outside of conditional directive block",
-                        `${ltoken.filename}:${token.line}:${token.column}`
-                    );
-                }
-                this.allowSubstitutions = true;
-                // hide everything
-                token.channel = directiveToken.channel = LPCLexer.HIDDEN;
-                directiveTokens.forEach((t) => {
-                    t.channel = token.channel;
-                });
-                return true;
+                return this.processElse(token, directiveToken, directiveTokens);
             case LPCLexer.ENDIF:
-                this.conditionalStack.pop();
-                // set this back to NOT disabled
-                token.channel = directiveToken.channel =
-                    LPCLexer.DEFAULT_TOKEN_CHANNEL;
-                // hide everything
-                token.channel = directiveToken.channel = LPCLexer.HIDDEN;
-                directiveTokens.forEach((t) => {
-                    t.channel = token.channel;
-                });
-                return true;
+                return this.processEndif(
+                    token,
+                    directiveToken,
+                    directiveTokens
+                );
             case LPCLexer.IFDEF:
             case LPCLexer.IFNDEF:
-                this.allowSubstitutions = true;
-                const conditionalTokens = directiveTokens;
-                if (this.inConditional && !this.isExecutable) {
-                    this.conditionalStack.push(ConditionalState.Ignored);
-                    // disable this entire conditional
-                    token.channel = directiveToken.channel = DISABLED_CHANNEL;
-                    conditionalTokens.forEach((t) => {
-                        token.channel = DISABLED_CHANNEL;
-                    });
-                } else {
-                    const shouldExist =
-                        token.type == LPCLexer.IFDEF ? true : false;
-                    const conditionalTokens = directiveTokens;
-                    conditionalTokens.shift(); // remove the space
-                    const symName = conditionalTokens
-                        .map((t) => t.text)
-                        .join(" ")
-                        .trim();
-
-                    this.conditionalStack.push(
-                        this.macroTable.has(symName) === shouldExist
-                            ? ConditionalState.Enabled
-                            : ConditionalState.Disabled
-                    );
-                }
-                this.allowSubstitutions = false;
-
-                // hide everything
-                token.channel = directiveToken.channel = LPCLexer.HIDDEN;
-                directiveTokens.forEach((t) => {
-                    t.channel = token.channel;
-                });
-
-                return true;
+                return this.processIfdef(
+                    token,
+                    directiveToken,
+                    directiveTokens
+                );
         }
 
         if (!this.isExecutable) {
@@ -349,106 +493,23 @@ export class LPCPreprocessingLexer extends LPCLexer {
 
         switch (directiveToken.type) {
             case LPCLexer.INCLUDE:
-                directiveTokens.shift(); // remove the space
-                const includeFilename = directiveTokens
-                    .map((t) => t.text)
-                    .join(" ")
-                    .trim();
-
-                if (!this.fileHandler) {
-                    console.warn(
-                        `Could not load ${includeFilename}, no file handler set.`
-                    );
-                }
-
-                const includeFile =
-                    this.fileHandler.loadImport(includeFilename);
-
-                (
-                    this.macroLexer.tokenFactory as LPCTokenFactor
-                ).filenameStack.push(includeFile.uri);
-
-                // lex the the include file
-                this.macroLexer.inputStream = CharStream.fromString(
-                    includeFile.source
+                return this.processInclude(
+                    token,
+                    directiveToken,
+                    directiveTokens
                 );
-                this.macroLexer.reset();
-                const includeTokens = this.macroLexer.getAllTokens();
-
-                // check if last token is EOF and remove it
-                if (
-                    includeTokens[includeTokens.length - 1].type == LPCLexer.EOF
-                ) {
-                    includeTokens.pop();
-                }
-
-                // push via emit to ensure that the tokens are processed by the preprocessor
-                this.isConsumingDirective = false;
-                this.allowSubstitutions = true;
-
-                this.emitQueue.push(...includeTokens);
-                // includeTokens.forEach((t) => {
-                //     this.emitToken(t);
-                // });
-
-                (
-                    this.macroLexer.tokenFactory as LPCTokenFactor
-                ).filenameStack.pop();
-                return true;
             case LPCLexer.DEFINE:
-                const defValToken = directiveTokens.shift()!;
-                const defVal = defValToken.text.trim();
-                // macro name is everything up to the first space
-                const nameEndIndex = defVal.match(/[(\s]/)?.index;
-                const spaceIndex = defVal.indexOf(" ");
-                const openParenIndex = defVal.indexOf("(");
-                const closingParenIndex = defVal.indexOf(")");
-
-                const macroName = defVal.substring(0, nameEndIndex);
-
-                // macro value is everything after the first space
-                const isFn = closingParenIndex >= 0;
-                const macroValue = defVal.substring(
-                    defVal.indexOf(" ", closingParenIndex)
+                return this.processDefine(
+                    token,
+                    directiveToken,
+                    directiveTokens
                 );
-
-                const def: MacroDefinition = {
-                    value: macroValue,
-                    name: macroName,
-                    filename: "macro.h",
-                    annotation: "",
-                    args: isFn ? [] : undefined,
-                };
-
-                // lex the body
-                this.macroLexer.inputStream = CharStream.fromString(macroValue);
-                this.macroLexer.reset();
-                def.bodyTokens = this.macroLexer.getAllTokens();
-
-                if (isFn) {
-                    def.argIndex = new Map();
-
-                    // lex the args
-                    const args = defVal
-                        .substring(openParenIndex + 1, closingParenIndex)
-                        .split(",");
-                    args.forEach((a, i) => {
-                        def.argIndex.set(a.trim(), i);
-                    });
-                    // prob don't need this:
-                    def.args = args.map((a) => a.trim());
-                }
-
-                this.macroTable.set(macroName, def);
-
-                return true;
             case LPCLexer.UNDEF:
-                const spaceToken = directiveTokens.shift()!;
-                const undefToken = directiveTokens.shift()!;
-                const undefMacroName = undefToken.text.trim();
-                this.macroTable.delete(undefMacroName);
-
-                return true;
+                return this.processUndefine(
+                    token,
+                    directiveToken,
+                    directiveTokens
+                );
         }
     }
 
