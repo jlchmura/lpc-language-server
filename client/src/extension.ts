07ce187955ae52eb9193b5a52593af5270ec2c68
jlchmura <jlchmura@gmail.com>
2024-06-28 12:02:46 -0400

    remove expose-gc flag

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -45,7 +45,7 @@ export function activate(context: ExtensionContext) {
     // get location of efuns folder and pass to server as an argument
     const efunDir = context.asAbsolutePath("efuns");
 
-    let debugOptions = { execArgv: ["--nolazy", "--inspect", "--expose-gc"] };
+    let debugOptions = { execArgv: ["--nolazy", "--inspect"] };
 
     // If the extension is launched in debug mode then the debug server options are used
     // Otherwise the run options are used
@@ -53,7 +53,7 @@ export function activate(context: ExtensionContext) {
         run: {
             module: serverModule,
             transport: TransportKind.ipc,
-            options: { execArgv: ["--expose-gc"] },
+            options: { execArgv: [] },
             args: [efunDir],
         },
         debug: {
