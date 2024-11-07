8375932c65a11cc88db67652edfe73788bed5f6b
jlchmura <jlchmura@gmail.com>
2024-11-07 10:58:54 -0500

    unit tests

--- server/src/tests/cases/compiler/operatorDoubleBang.c
+++ server/src/tests/cases/compiler/operatorDoubleBang.c
@@ -1,4 +1,14 @@
+// @driver: fluffos
+// various forms of fluff's double-bang operator
+reset() { return 0; }
+
 test() {
-    int i=1;
-    int f = !!i;
+    int i = 0;
+    if (!!i) {
+        write("i is not zero");
+    }
+    int j = !!(i==1);
+    int k = !!reset();
+    int l = !reset(); 
+    int m = !!"something";
 }
