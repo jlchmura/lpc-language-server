1015a6c8e82989bf4db78dac0da947eaab31bcc2
jlchmura <jlchmura@gmail.com>
2024-12-26 18:17:31 -0500

    working cli

--- server/src/bin/lpc.ts
+++ server/src/bin/lpc.ts
@@ -32,7 +32,6 @@ const canonicalConfigFilePath = (toCanonicalFileName(configFileName)) as lpc.ser
 
 lpc.sys.write(`Using config file: ${canonicalConfigFilePath}\n`);
 
-
 const parsedConfig = lpc.getParsedCommandLineOfConfigFile(configFileName, {}, {
     ...lpc.sys,
     getCurrentDirectory: () => lpc.sys.getCurrentDirectory(),
@@ -51,15 +50,60 @@ const createProgramOptions: lpc.CreateProgramOptions = {
     options: compilerOptions,                
     oldProgram: undefined,                
 };
-            
+
+// our console output is still verbose, so mask console output
+console.log = lpc.noop;
+console.debug = lpc.noop;
+console.info = lpc.noop;
+console.warn = lpc.noop;
+
+function defaultIsPretty(sys: lpc.System) {
+    return !!sys.writeOutputIsTTY && sys.writeOutputIsTTY() && !sys.getEnvironmentVariable("NO_COLOR");
+}
+
+function shouldBePretty(sys: lpc.System, options: lpc.CompilerOptions | lpc.BuildOptions) {
+    if (!options || typeof options.pretty === "undefined") {
+        return defaultIsPretty(sys);
+    }
+    return options.pretty;
+}
+
+function updateReportDiagnostic(
+    sys: lpc.System,
+    existing: lpc.DiagnosticReporter,
+    options: lpc.CompilerOptions | lpc.BuildOptions,
+): lpc.DiagnosticReporter {
+    return shouldBePretty(sys, options) ?
+        lpc.createDiagnosticReporter(sys, /*pretty*/ true) :
+        existing;
+}
+
+const reportDiagnostic = updateReportDiagnostic(
+    lpc.sys,
+    lpc.createDiagnosticReporter(lpc.sys),
+    compilerOptions
+);
+
 // create program and get file we are trying to compile
 const program = lpc.createProgram(createProgramOptions);
+
+const diags: lpc.DiagnosticWithLocation[] = [];
+
 program.getRootFileNames().forEach(f => {
     const sourceFile = program.getSourceFile(f);
     const parseDiags = sourceFile.parseDiagnostics;
-    const semanticDiags = program.getSemanticDiagnostics(sourceFile);    
-    const diags = parseDiags.concat(semanticDiags);
-    lpc.sys.write(`Root file: ${f} (${diags.length})\n`);
+    diags.push(...parseDiags);
+    
+    if (compilerOptions.diagnostics) {
+        const semanticDiags = program.getSemanticDiagnostics(sourceFile);
+        diags.push(...semanticDiags);
+    }
+
+    diags.forEach(d => {
+        reportDiagnostic(d);
+    });
 });
 
-
+lpc.sys.write("\n");
+const diagTxt = lpc.getErrorSummaryText(diags.length, lpc.getFilesInErrorForSummary(diags), "\n", compilerHost);
+lpc.sys.write(diagTxt);    
