2acd24b87a3a568b72e935cb7f1caef1eba23945
jlchmura <jlchmura@gmail.com>
2025-03-13 13:22:01 -0400

    propertly check array element type in foreach - closes #243 - add unit test for 243 - improve diagnostic message

---
+++ server/src/tests/cases/compiler/forEach11.c
@@ -0,0 +1,12 @@
+test() {
+    string* functionTypes;
+    string funcType;
+
+    foreach(funcType in functionTypes) {
+        
+    }
+}
+
+/**
+ * tests for https://github.com/jlchmura/lpc-language-server/issues/243
+ */
